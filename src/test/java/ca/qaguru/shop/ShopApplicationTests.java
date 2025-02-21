872ae281a9b7bd3483d7e8e6d637c648fd150cf2
pradeep <qaguru.update@gmail.com>
2025-02-20 23:27:39 -0500

    added security

--- src/test/java/ca/qaguru/shop/ShopApplicationTests.java
+++ src/test/java/ca/qaguru/shop/ShopApplicationTests.java
@@ -1,13 +1,13 @@
-package ca.qaguru.shop;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ShopApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package ca.qaguru.shop;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ShopApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
